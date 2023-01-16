a99777fb896796d4f45e97625d26eb7b8b749b19
umair shah <umairshah14@hotmail.com>
2023-01-16 21:29:07 +0000

    edits made

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,4 +1,4 @@
-    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) 
     var highscoresItem = document.createElement("li");
     highscoresItem.textContent = "Score: " + highscores.score + " | Initials: " + highscores.initials
     console.log(highscoresItem);
